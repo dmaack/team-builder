f4356b5047427f5515efbe736ee81c5863bdc978
dominique maack <dominiquemaack@gmail.com>
2019-09-18 18:00:53 -0600

    Fixed error and everything displaying as expected

--- src/components/Member.js
+++ src/components/Member.js
@@ -1,20 +1,20 @@
-// import React from 'react';
+import React from 'react';
 
-// const Member = props => {
+const Member = props => {
 
-//     return(
-//         <div>
-//            {props.memberList.map(member => {
-//                return(
-//                    <div className='member' key={member.id}>
-//                        <h2>{member.name}</h2>
-//                        <p>{member.email}</p>
-//                        <p>{member.role}</p>
-//                    </div>
-//                )
-//            })} 
-//         </div>
-//     )
-// }
+    return(
+        <div>
+           {props.members.map(member => (
+              
+                   <div className='member' key={member.id}>
+                       <h2>{member.name}</h2>
+                       <p>{member.email}</p>
+                       <p>{member.role}</p>
+                   </div>
+               
+           ))} 
+        </div>
+    )
+}
 
-// export default Member;
+export default Member;
--- src/components/MemberForm.js
+++ src/components/MemberForm.js
@@ -3,23 +3,24 @@ import React, { useState } from 'react';
 const MemberForm = props => {
     const [member, setMember] = useState({name: '', email: '', role: ''});
     const handleChanges = e => {
-        setMember({...member, [e.target.name]: e.target.value, [e.target.email]: e.target.value, [e.target.role]: e.target.value})
+        setMember({...member, [e.target.name]: e.target.value})
     }
 
     //creating our new member
     const submitForm = e => {
         e.preventDefault();
         props.addNewMember(member);
+        //clears out the input values
         setMember({name: '', email: '', role: ''});
     }
     return(
         <form onSubmit={submitForm}>
             <label htmlFor='member-name'>Name:</label>
-            <input id='member-name' type='text' onChange={handleChanges} value={member.name}></input>
+            <input id='member-name' type='text' name='name' onChange={handleChanges} value={member.name}></input>
             <label htmlFor='member-email'>Email:</label>
-            <input id='member-email' type='text' onChange={handleChanges} value={member.email}></input>
+            <input id='member-email' type='text' name='email' onChange={handleChanges} value={member.email}></input>
             <label htmlFor='member-role'>Role:</label>
-            <input id='member-role' type='text' onChange={handleChanges} value={member.role}></input>
+            <input id='member-role' type='text' name='role' onChange={handleChanges} value={member.role}></input>
             <button type='submit'>Add Member</button>
         </form>
     )
